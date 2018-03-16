932564cf54db9d91487136fa79a81bde7ab5e8cb
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-16 18:58:32 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,13 @@
 class Backer
-  attr_reader :name
+  attr_reader :name, :back_project
 
   def initialize(name)
     @name = name
+    back_project = []
   end
+
+  def back_project(project)
+    @back_project << project
+  end
+    
 end
